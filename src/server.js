147fca472fb134c37419641ed70c64a974c3584a
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-14 22:02:16 -0300

    feat: initial commit

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('./api');
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
 
-// não remova esse endpoint
+
 app.get('/', (_request, response) => {
   response.send();
 });
