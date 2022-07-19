71ed64fef85cf9321ea3ba6febf2113bfb5029b6
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-18 23:38:54 -0300

    🔨feat: Migration Categories (up and dropTable)

--- src/database/migrations/20220719021543-Categories.js
+++ src/database/migrations/20220719021543-Categories.js
@@ -18,11 +18,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Categories')
   }
 };
