87d82ecf151aabe10fbb4cd5f364c4a9ed0ccacc
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-18 21:51:57 -0300

    🔨feat: first Migration (table Users)

---
+++ src/database/migrations/20220719003121-createUsers.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  // Migration para criar a tabela users
+  up: async (queryInterface, Sequelize) => {
+   queryInterface.createTable('users',
+   {
+    id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey:true,
+    autoIncrement:true,
+   },
+    displayName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    }
+  }
+   )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
