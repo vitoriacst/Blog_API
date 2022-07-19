bf6dfdfeaa0f9081d2f09d056c7124717f7136e2
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-18 23:50:27 -0300

    🔨feat: Migration PostCategories (down table)

--- src/database/migrations/20220719024439-PostCategories.js
+++ src/database/migrations/20220719024439-PostCategories.js
@@ -21,11 +21,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users')
   }
 };
