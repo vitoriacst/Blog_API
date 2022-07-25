803c71e582e7c07c143a394f60e0614bc7fd6634
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-25 00:20:06 -0300

    🔨feat: endpoint POST /categories em andamento(services)

--- src/service/Category.services.js
+++ src/service/Category.services.js
@@ -1,3 +1,19 @@
 const { category } = require('../database/models/index');
 
-const CategoryName = async () => {};
+const CategoryName = async (name) => {
+  try {
+    const response = await category.create(name, {
+      raw: true,
+    });
+    return response;
+  } catch (error) {
+    return {
+      error: {
+        code: 'required',
+        message: '/name/ is required',
+      },
+    };
+  }
+};
+
+module.exports = { CategoryName };
